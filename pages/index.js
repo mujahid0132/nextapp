fd54e7d8786dfb2725ec3ef5453ad955a7eed89b
Your Name <ludosheikh@gmail.com>
2023-08-22 10:57:24 +0500

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,79 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import { useState, useRef, useEffect } from "react";
+import gsap from 'gsap'
+// let inter = Inter({ subsets: ['latin'] })
+// import '@/styles/globals.css'
 export default function Home() {
+
+  let tl = gsap.timeline()
+
+  let bgrotatable = useRef(null);
+  let content = useRef(null);
+  let [side, setside] = useState(true); //true is firstside and false is second side
+  let [zindex, setzindex] = useState(true);
+  let animationtime = 2000; //in milliseconds
+  let timetoreloadnextpage = 2;
+  let gotosidetwo = () => {
+    tl
+      .to(".box", { scale: 0.8, duration: 0.5 })
+      .to(".box", { rotateY: 90, duration: 0.4 })
+      .to(".box", { rotateY: 180, duration: 2 , delay: timetoreloadnextpage})
+      .to(".loader",{ opacity: 0, duration: 0.2 },timetoreloadnextpage)
+      .to(".box", { scale: 1, duration: 0.1, ease: "power4.out" })
+      .to(".secondelement", { scale: 10, duration: 1, ease: "power4.out" })
+
+  };
+  let changeside = () => {
+    side ? setside(false) : setside(true);
+    bgrotatable.current.style.animation = side
+      ? gotosidetwo()
+      : `animationsidechange-reverse ${animationtime}ms forwards`;
+    setTimeout(() => {
+      side ? setzindex(false) : setzindex(true);
+    }, animationtime / 2);
+  }
+
+
+  // useEffect(() => {
+  // tl.to(".box", { x: 200 })
+  //   .to(".box", { y: 200 },0.1)
+  // }, [])
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+    <div className="w-full h-screen flex items-center justify-center">
+
+    <div className="w-20 aspect-square rounded-full border-t border-t-black animate-spin loader"></div>
+    </div>
+      <button
+        onClick={changeside}
+        className="border border-black overflow-hidden px-4 py-2 rounded-full capitalize transition-all duration-500 absolute top-10 right-10 z-50"
+      >
+        go to {side ? "second" : "first"} side
+      </button>
+
+      <div
+        ref={bgrotatable}
+        className={`h-screen w-full fixed top-0 left-0 overflow-hidden box`}
+      >
+        <div
+          className={`${zindex ? "z-10" : "-z-10"
+            } absolute h-full w-full bg-pink-500`}
+        >
+          first
+        </div>
+        <div
+          className={`${zindex ? "-z-10" : "z-10"
+            } absolute h-full w-full bg-blue-600 flex items-center justify-center`}
+          style={{ transform: "rotateY(180deg)" }}
+        >
+          {/* <span className=" uppercase font-extrabold text-5xl opacity-0 secondelement">
+          second
+          </span> */}
+          <span className="w-10 aspect-square rounded-full bg-green-500 secondelement">
+          </span>
+
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   )
 }
